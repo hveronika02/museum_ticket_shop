84a369b52dfd390e933bd4323a9c210b702d3bde
Horvath Veronika <hveronika0202@gmail.com>
2025-05-19 21:43:30 +0200

    Fix authguard bug

--- src/app/services/auth.guard.ts
+++ src/app/services/auth.guard.ts
@@ -4,7 +4,7 @@ import { CanActivateFn, Router } from '@angular/router';
 export const authGuard: CanActivateFn = (route, state) => {
   const router = inject(Router);
   const user = JSON.parse(localStorage.getItem('user') as string) ;
-  if (user.length == 0) {
+  if (user == null || user.length == 0) {
     return true;
   } else {
     // Ha nincs user, irányítsuk át a login oldalra
